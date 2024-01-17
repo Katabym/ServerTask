c7add997aa63fb41dc09d815e0fdfc10a8f7dd32
Yaro Bor <borovinscky.yaroslaw@ya.ru>
2024-01-17 20:10:25 +0300

    Upload solution

---
+++ solution/App2/app2.py
@@ -0,0 +1,41 @@
+import asyncio
+import time
+from nats.aio.client import Client as NATS
+
+async def handler(msg):
+    # Получаем реальное время в тик и
+    # Округляем время до мс
+    t_real = time.time()
+    t_real_rounded = float('{:.4f}'.format(t_real))
+
+    # Получаем t_sign из сообщения
+    t_sign = float(msg.data.decode())
+
+    # Вычисляем dt и округляем его
+    dt = t_real_rounded - t_sign
+    t_dt_rounded = float('{:.4f}'.format(dt))
+
+    # Выводим dt в консоль
+    print(f"dt = {t_dt_rounded}")
+
+async def pub_sub_loop():
+    # Подключаемся к локальному серверу NATS на порт 4222
+    nc = NATS()
+    await nc.connect(servers=["nats://localhost:4222"])
+
+    # Подписываюсь на топик от первого приложения и
+    # указываю обработчик  полученного сообщения
+    await nc.subscribe("app1_tick", cb=handler)
+
+    while True:
+        # Получаем реальное время в тик и
+        # Округляем время до мс
+        t_real = time.time()
+        t_rounded = float('{:.4f}'.format(t_real))
+
+        # Публикуем сообщение с округленным временем в шину данных
+        await nc.publish("app2_tick", str(t_rounded).encode())
+        # Ставлю задержку между публикациями в 10 мс
+        await asyncio.sleep(0.01)
+
+asyncio.run(pub_sub_loop())
